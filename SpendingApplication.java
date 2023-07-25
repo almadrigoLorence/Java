b1ba60e80c994f8fdd582b5aeca6f38922c77248
Lorence <131726849+almadrigoLorence@users.noreply.github.com>
2023-07-25 14:39:10 +0800

    Add files via upload

---
+++ SpendingApplication.java
@@ -0,0 +1,34 @@
+package SpendingApplication;
+
+import java.util.Scanner;
+/*
+    Spending Application to help examine the spending
+    patterns of a user.
+    Date: 02/06/2023
+ */
+public class SpendingApplication {
+
+//    Calculate the expense method
+    public static void main(String[] args){
+        double food, clothing, entertainment, rent;
+        Scanner input = new Scanner(System.in);
+
+//      User must input a number given by the instruction
+        System.out.println("Enter the amount spent last month on the following Items");
+        System.out.print("Food: ");
+        food = input.nextDouble();
+        System.out.print("Clothing: ");
+        clothing = input.nextDouble();
+        System.out.print("Entertainment: ");
+        entertainment = input.nextDouble();
+        System.out.print("Rent: ");
+        rent = input.nextDouble();
+        input.close();
+
+        System.out.print("Overall Total: "+"$"+CalculateExpense.calculateExpense(food, clothing, entertainment, rent));
+
+        System.out.println("");
+        CalculateExpense.printTable(food, clothing, entertainment, rent);
+
+    }
+}
